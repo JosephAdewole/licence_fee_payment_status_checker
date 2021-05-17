7c1f1ce97d420be9414995bb52e3ed6ee70a70ea
IamNator <natverior1@gmail.com>
2021-05-17 18:26:47 +0100

    ...

--- internal/database/subscribers.go
+++ internal/database/subscribers.go
@@ -17,17 +17,17 @@ type Subscriber struct {
 //AddUpdate adds or update subcriber record to database
 func (sub *Subscriber) AddUpdate(db *gorm.DB) error {
 
-	num := db.Model(&Subscriber{}).Create(&sub)
+	num := db.Model(&Subscriber{}).Create(sub)
 
 	if num.RowsAffected < int64(1) {
-		e := db.Model(&Subscriber{}).Where("plate_number = ?", sub.PlateNumber).UpdateColumn(&sub)
-		db.Model(&Subscriber{}).Where("plate_number = ?", sub.PlateNumber).First(&sub)
+		e := db.Model(&Subscriber{}).Where("plate_number = ?", sub.PlateNumber).UpdateColumn(sub)
+		db.Model(&Subscriber{}).Where("plate_number = ?", sub.PlateNumber).First(sub)
 		if e.Error != nil {
 			return e.Error
 		}
 	}
 
-	num.Where("plate_number = ?", sub.PlateNumber).First(&sub)
+	num.Where("plate_number = ?", sub.PlateNumber).First(sub)
 
 	return nil
 }
