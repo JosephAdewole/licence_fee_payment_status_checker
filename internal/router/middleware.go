3b072b9b855485fd74f302bca1a5630be1612725
IamNator <natverior1@gmail.com>
2021-05-17 22:28:40 +0100

    added cors

--- internal/router/middleware.go
+++ internal/router/middleware.go
@@ -1,3 +1,16 @@
 package router
 
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+//Cors handles cors
+func Cors(c *gin.Context) {
+	if c.Request.Method == http.MethodOptions {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		return
+	}
+	c.Next()
+}
