28f659e3e3d2d627e610613d3f080c0b3e366855
IamNator <natverior1@gmail.com>
2021-05-20 00:31:14 +0100

    updated the addupdate checks database

--- internal/database/checks.go
+++ internal/database/checks.go
@@ -10,8 +10,8 @@ import (
 //Check is record of a check done by bot (rasp berry pi robot)
 type Check struct {
 	ID             uint      `json:"id" gorm:"primary_key"`
-	IsEmpty        *bool     `json:"is_empty"`
-	PlateNumber    *string   `json:"plate_number"`
+	IsEmpty        bool      `json:"is_empty"`
+	PlateNumber    string    `json:"plate_number"`
 	PackingSpaceID uint      `json:"packing_space_id" gorm:"uniqueIndex"`
 	CreatedAt      time.Time `json:"created_at"`
 }
